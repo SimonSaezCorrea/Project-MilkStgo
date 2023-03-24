23eec4b02d0ac899b98151c4d2ac06f8ff736a47
Simon Saez <nayxxaxx789super@gmail.com>
2023-03-24 12:38:45 -0300

    Creación del proyecto
    Inicio del proyecto

---
+++ src/main/java/com/example/Project/MilkStgo/ProjectMilkStgoApplication.java
@@ -0,0 +1,13 @@
+package com.example.Project.MilkStgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectMilkStgoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectMilkStgoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Project/MilkStgo/ProjectMilkStgoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Project.MilkStgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectMilkStgoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
