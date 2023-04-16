6647b6dd3ee69a71f8670dbfcb0be252466bb9df
Simon Saez <nayxxaxx789super@gmail.com>
2023-04-16 00:58:12 -0400

    Se modifico la vista de plantilla y el controlador
    Se modificó la vista de plantilla para hacerlo más comodo de manejar en programación y en consecuencia se modificó el controlador

--- src/main/java/Mingeso/ProjectMilkStgo/controllers/PlanillaPagosController.java
+++ src/main/java/Mingeso/ProjectMilkStgo/controllers/PlanillaPagosController.java
@@ -4,24 +4,22 @@ import Mingeso.ProjectMilkStgo.entities.AcopioLecheEntity;
 import Mingeso.ProjectMilkStgo.entities.GrasaSolidoTotalEntity;
 import Mingeso.ProjectMilkStgo.entities.ProveedorEntity;
 import Mingeso.ProjectMilkStgo.entities.QuincenasEntity;
+import Mingeso.ProjectMilkStgo.repositories.ProveedorRepository;
 import Mingeso.ProjectMilkStgo.repositories.QuincenasRepository;
 import Mingeso.ProjectMilkStgo.services.AcopioLecheService;
 import Mingeso.ProjectMilkStgo.services.GrasaSolidoTotalesService;
 import Mingeso.ProjectMilkStgo.services.ProveedorService;
 import Mingeso.ProjectMilkStgo.services.QuincenasService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import java.time.Instant;
-import java.time.LocalDateTime;
 import java.util.*;
-import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping
@@ -37,63 +35,73 @@ public class PlanillaPagosController {
     private QuincenasService quincenasService;
     @Autowired
     private QuincenasRepository quincenasRepository;
+    @Autowired
+    private ProveedorRepository proveedoresRepository;
 
     @GetMapping("/planilla_de_pagos")
-    public String main(){
+    public String main(Model model){
+        List<ProveedorEntity> proveedorEntityList = proveedoresRepository.findAll();
+        List<QuincenasEntity> quincenasEntityList = new ArrayList<>();
+        for(ProveedorEntity proveedorEntity:proveedorEntityList){
+            quincenasEntityList.add(quincenasRepository.encontrarUltimo(proveedorEntity.getProveedor_id()));
+        }
+        int contador = 0;
+        for(QuincenasEntity quincenasEntity:quincenasEntityList){
+            if(quincenasEntity==null){
+                quincenasEntityList.set(contador, new QuincenasEntity());
+                quincenasEntityList.get(contador).setFecha("-");
+                quincenasEntityList.get(contador).setProveedor_id(proveedorEntityList.get(contador).getProveedor_id());
+                quincenasEntityList.get(contador).setNombreProveedor(proveedorEntityList.get(contador).getNombre());
+            }
+            contador++;
+        }
+        model.addAttribute("quincenasEntityList", quincenasEntityList);
         return "planilla_de_pagos";
     }
 
     @PostMapping("/planilla_de_pagos")
-    public String nuevaQuincena(@RequestParam("codigo") String proveedor_id){
+    public String nuevaQuincena(@RequestParam("proveedor_id") String proveedor_id, RedirectAttributes redirectAttributes){
         ProveedorEntity proveedorEntity = proveedorService.encontrarPorCodigo(proveedor_id);
         List<AcopioLecheEntity> acopioLecheEntityList = acopioLecheService.obtenerAcopioLeche(proveedor_id);
-        GrasaSolidoTotalEntity grasaSolidoTotalEntity = grasaSolidoTotalesService.obtenerGrasaSolidoTotal(proveedor_id);
-        QuincenasEntity quincenasEntity = new QuincenasEntity();
-        Calendar fecha = Calendar.getInstance();
-        String fechaUnida = fecha.get(Calendar.YEAR) +"/"+ fecha.get(Calendar.MONTH) +"/"+ fecha.get(Calendar.DATE);
-        quincenasEntity.setFecha(fechaUnida);
-        quincenasEntity.setProveedor_id(proveedor_id);
-        quincenasEntity.setNombreProveedor(proveedorEntity.getNombre());
-        int sum = 0;
-        for(AcopioLecheEntity acopioLecheEntity:acopioLecheEntityList){
-            sum+=Integer.parseInt(acopioLecheEntity.getKls_leche());
+        System.out.println(acopioLecheEntityList);
+        if(acopioLecheEntityList.isEmpty()){
+            redirectAttributes.addFlashAttribute("mensaje", "No ha sido posible calcular");
+            System.out.println("es nulo");
+        }else {
+            System.out.println("no es nulo");
+            GrasaSolidoTotalEntity grasaSolidoTotalEntity = grasaSolidoTotalesService.obtenerGrasaSolidoTotal(proveedor_id);
+            QuincenasEntity quincenasEntity = new QuincenasEntity();
+            Calendar fecha = Calendar.getInstance();
+            String fechaUnida = fecha.get(Calendar.YEAR) + "/" + fecha.get(Calendar.MONTH) + "/" + fecha.get(Calendar.DATE);
+            quincenasEntity.setFecha(fechaUnida);
+            quincenasEntity.setProveedor_id(proveedor_id);
+            quincenasEntity.setNombreProveedor(proveedorEntity.getNombre());
+            int sum = 0;
+            for (AcopioLecheEntity acopioLecheEntity : acopioLecheEntityList) {
+                sum += Integer.parseInt(acopioLecheEntity.getKls_leche());
+            }
+            quincenasEntity.setKlsLeche(sum);
+            quincenasEntity.setNumeroDiasLeche(acopioLecheEntityList.size());
+            quincenasEntity.setPromedioKlsLeche(sum / acopioLecheEntityList.size());
+            quincenasEntity.setVariacionLeche(quincenasService.descuentoVariacionLeche(acopioLecheEntityList));
+            quincenasEntity.setGrasa(Integer.parseInt(grasaSolidoTotalEntity.getGrasa()));
+            quincenasEntity.setVariacionGrasa(quincenasService.descuentoVariacionGrasa(grasaSolidoTotalEntity));
+            quincenasEntity.setSolido(Integer.parseInt(grasaSolidoTotalEntity.getSolidoTotal()));
+            quincenasEntity.setVariacionSolido(quincenasService.descuentoVariacionSolidoTotal(grasaSolidoTotalEntity));
+            quincenasEntity.setPagoLeche(quincenasService.sueldoCategoria(acopioLecheEntityList, proveedorEntity));
+            quincenasEntity.setPagoGrasa(quincenasService.sueldoGrasa(acopioLecheEntityList, grasaSolidoTotalEntity));
+            quincenasEntity.setPagoSolido(quincenasService.sueldoSolido(acopioLecheEntityList, grasaSolidoTotalEntity));
+            quincenasEntity.setBonificacion(quincenasService.bonificacionFrecuencia(acopioLecheEntityList));
+            quincenasEntity.setDescuentoLeche(quincenasService.descuentoVariacionLeche(acopioLecheEntityList));
+            quincenasEntity.setDescuentoGrasa(quincenasService.descuentoVariacionGrasa(grasaSolidoTotalEntity));
+            quincenasEntity.setDescuentoSolido(quincenasService.descuentoVariacionSolidoTotal(grasaSolidoTotalEntity));
+            quincenasEntity.setPagoTotal(quincenasService.pagoAcopioLeche(grasaSolidoTotalEntity, proveedorEntity, acopioLecheEntityList));
+            quincenasEntity.setRetencion(quincenasService.retencion(quincenasEntity.getPagoTotal()));
+            quincenasEntity.setMontoFinal(quincenasService.pagoFinal(grasaSolidoTotalEntity, proveedorEntity, acopioLecheEntityList));
+            quincenasService.guardarQuincena(quincenasEntity);
+            redirectAttributes.addFlashAttribute("mensaje", "Se ha calculado correctamente");
         }
-        quincenasEntity.setKlsLeche(sum);
-        quincenasEntity.setNumeroDiasLeche(acopioLecheEntityList.size());
-        quincenasEntity.setPromedioKlsLeche(sum/acopioLecheEntityList.size());
-        quincenasEntity.setVariacionLeche(quincenasService.descuentoVariacionLeche(acopioLecheEntityList));
-        quincenasEntity.setGrasa(Integer.parseInt(grasaSolidoTotalEntity.getGrasa()));
-        quincenasEntity.setVariacionGrasa(quincenasService.descuentoVariacionGrasa(grasaSolidoTotalEntity));
-        quincenasEntity.setSolido(Integer.parseInt(grasaSolidoTotalEntity.getSolidoTotal()));
-        quincenasEntity.setVariacionSolido(quincenasService.descuentoVariacionSolidoTotal(grasaSolidoTotalEntity));
-        quincenasEntity.setPagoLeche(quincenasService.sueldoCategoria(acopioLecheEntityList, proveedorEntity));
-        quincenasEntity.setPagoGrasa(quincenasService.sueldoGrasa(acopioLecheEntityList, grasaSolidoTotalEntity));
-        quincenasEntity.setPagoSolido(quincenasService.sueldoSolido(acopioLecheEntityList, grasaSolidoTotalEntity));
-        quincenasEntity.setBonificacion(quincenasService.bonificacionFrecuencia(acopioLecheEntityList));
-        quincenasEntity.setDescuentoLeche(quincenasService.descuentoVariacionLeche(acopioLecheEntityList));
-        quincenasEntity.setDescuentoGrasa(quincenasService.descuentoVariacionGrasa(grasaSolidoTotalEntity));
-        quincenasEntity.setDescuentoSolido(quincenasService.descuentoVariacionSolidoTotal(grasaSolidoTotalEntity));
-        quincenasEntity.setPagoTotal(quincenasService.pagoAcopioLeche(grasaSolidoTotalEntity, proveedorEntity, acopioLecheEntityList));
-        quincenasEntity.setRetencion(quincenasService.retencion(quincenasEntity.getPagoTotal()));
-        quincenasEntity.setMontoFinal(quincenasService.pagoFinal(grasaSolidoTotalEntity, proveedorEntity, acopioLecheEntityList));
-        quincenasService.guardarQuincena(quincenasEntity);
         return "redirect:/planilla_de_pagos";
     }
 
-    @GetMapping("/planilla_de_pagos/{proveedor_id}")
-    public String mostrarQuincenas(@RequestParam("proveedor_id") String proveedor_id,
-                                                         Model model){
-        List<QuincenasEntity> quincenasList= quincenasRepository.encontrarTodos(proveedor_id);
-        List<String> fechasList = quincenasList.stream().map(QuincenasEntity::getFecha).distinct().collect(Collectors.toList());
-        model.addAttribute("fechasList", fechasList);
-        return "planilla_de_pagos";
-    }
-    @GetMapping("/planilla_de_pagos/{proveedor_id}/{fecha}")
-    public String mostrarQuincena(@RequestParam("proveedor_id") String proveedor_id,
-                                                           @RequestParam("fecha") String fecha,
-                                                           Model model){
-        QuincenasEntity quincenas = quincenasRepository.encontrarPorFechaYProveedor(proveedor_id, fecha);
-        model.addAttribute("quincenas", quincenas);
-        return "planilla_de_pagos";
-    }
 }
